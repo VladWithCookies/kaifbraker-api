e93b0ad2b9685d3673e86033e130c3e73c8f4595
Vlad V <summervibeswith9pm@gmail.com>
2019-12-14 13:21:53 +0200

    Add ability to update projects

--- app/graphql/mutations/update_project.rb
+++ app/graphql/mutations/update_project.rb
@@ -3,12 +3,11 @@ module Mutations
     argument :id, ID, required: true
     argument :title, String, required: false
     argument :description, String, required: false
-    argument :deadline, String, required: false
     argument :public, Boolean, required: false
 
     type Types::ProjectType
 
-    def resolve(id: nil, title: nil, description: nil, deadline: nil, public: nil)
+    def resolve(id: nil, title: nil, description: nil, public: nil)
       user = context[:current_user]
 
       return unless user
@@ -18,7 +17,6 @@ module Mutations
       project.update!(
         title: title,
         description: description,
-        deadline: deadline,
         public: public,
       )
 
